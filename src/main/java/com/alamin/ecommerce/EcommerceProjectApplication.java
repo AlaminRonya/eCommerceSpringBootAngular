9c1a5cee5c02412863fc680e902d15ab97760cb8
AlaminRonya <hprony12@gmail.com>
2023-01-19 20:37:40 +0600

    initial Angular

--- src/main/java/com/alamin/ecommerce/EcommerceProjectApplication.java
+++ src/main/java/com/alamin/ecommerce/EcommerceProjectApplication.java
@@ -1,7 +1,11 @@
 package com.alamin.ecommerce;
 
+import com.alamin.ecommerce.entity.ProductCategory;
+import com.alamin.ecommerce.repository.ProductCategoryRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EcommerceProjectApplication {
@@ -10,9 +14,19 @@ public class EcommerceProjectApplication {
         SpringApplication.run(EcommerceProjectApplication.class, args);
     }
 
+
+//    @Bean
+//    CommandLineRunner category(ProductCategoryRepository repository){
+//        return args -> {
+//            ProductCategory  category = new ProductCategory();
+//            category.setCategoryName("Java Advance");
+//            repository.save(category);
+//        };
+//    }
 }
 
 
 //('BOOK-TECH-1000', 'JavaScript - The Fun Parts', 'Learn JavaScript',
 //        'assets/images/products/placeholder.png'
 //        ,1,100,19.99,1, NOW());
+
